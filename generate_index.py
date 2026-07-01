90ab14300b8f378cbe74c037fbda904c4558c39a
wwwgoper77-wq <www.goper77@gmail.com>
2026-07-01 18:14:35 +0300

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -1,1 +1,22 @@
-import os, jsonplugins_dir = "plugins"data = {"store": "Enigma2 Store","plugins": []}if os.path.exists(plugins_dir):for f in os.listdir(plugins_dir):if f.endswith((".ipk", ".tar.gz", ".zip")):name = f.split(".")[0]data["plugins"].append({"name": name,"file": f"plugins/{f}","image": f"images/{name}.png","version": "1.0"})with open("index.json", "w") as x:json.dump(data, x, indent=4)
+import os
+import json
+
+plugins_dir = "plugins"
+data = {
+    "store": "Enigma2 Store",
+    "plugins": []
+}
+
+if os.path.exists(plugins_dir):
+    for f in os.listdir(plugins_dir):
+        if f.endswith(".ipk") or f.endswith(".tar.gz") or f.endswith(".zip"):
+            name = f.split("_")[0]
+            data["plugins"].append({
+                "name": name,
+                "file": f"plugins/{f}",
+                "image": f"images/{name}.png",
+                "version": 1.0
+            })
+
+with open("index.json", "w") as x:
+    json.dump(data, x, indent=4)
