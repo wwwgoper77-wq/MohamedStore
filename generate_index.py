e588a9f09086c4dbbf7dd37e7fbd83abb989d206
wwwgoper77-wq <www.goper77@gmail.com>
2026-07-21 18:54:20 +0300

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -43,10 +43,6 @@ except:
 def image_url(prefix):
     """
     Search automatically for an image beginning with prefix.
-    Example:
-    ipaudiopro.png
-    ipaudiopro-icon.png
-    ipaudiopro_v2.png
     """
     if not os.path.isdir("images"):
         return ""
@@ -61,46 +57,83 @@ def image_url(prefix):
 
 
 # صيغ ملفات التثبيت المدعومة بالكامل
-EXTENSIONS = (".ipk", ".sh", ".deb", ".zip", ".tar.gz", ".tgz", ".tar", ".py", ".tv")
+EXTENSIONS = (".ipk", ".sh", ".deb", ".zip", ".tar.gz", ".tgz", ".tar", ".py", ".tv", ".img", ".nfi")
+
+
+# -------------------------------------------------
+# Global Release Assets Fetcher (لجلب ملفات الـ Release وتوزيعها تلقائياً)
+# -------------------------------------------------
+
+release_assets_pool = []
+try:
+    api = f"https://api.github.com/repos/{GITHUB_USER}/{REPO_NAME}/releases"
+    req = urllib.request.Request(
+        api,
+        headers={
+            "User-Agent": "Mozilla/5.0",
+            "Accept": "application/vnd.github+json"
+        }
+    )
+    with urllib.request.urlopen(req, timeout=20) as response:
+        releases = json.loads(response.read().decode("utf-8"))
+
+    for release in releases:
+        for asset in release.get("assets", []):
+            filename = asset["name"]
+            if filename.endswith(EXTENSIONS):
+                release_assets_pool.append({
+                    "filename": filename,
+                    "url": asset["browser_download_url"]
+                })
+except Exception as e:
+    print("GitHub Releases Fetch Error:", e)
 
 
 # -------------------------------------------------
 # Plugins
 # -------------------------------------------------
 
+plugins_list = []
 if os.path.isdir("plugins"):
-
     for filename in sorted(os.listdir("plugins")):
+        if filename.endswith(EXTENSIONS):
+            plugins_list.append(filename)
 
-        if not filename.endswith(EXTENSIONS):
-            continue
+# إضافة ملفات البلجِن من الـ Release إن وجدت
+for asset in release_assets_pool:
+    fname = asset["filename"]
+    if "plugin" in fname.lower() or "ipa" in fname.lower() or "timeshift" in fname.lower() or "audi" in fname.lower():
+        if fname not in plugins_list:
+            plugins_list.append(fname)
 
-        if filename.endswith(".tar.gz"):
-            clean = filename[:-7]
-        else:
-            clean = os.path.splitext(filename)[0]
+for filename in sorted(plugins_list):
+    if filename.endswith(".tar.gz"):
+        clean = filename[:-7]
+    else:
+        clean = os.path.splitext(filename)[0]
 
-        version = clean.split("_")[-2] if "_" in clean else "1.0"
+    version = clean.split("_")[-2] if "_" in clean else "1.0"
+    display = clean.replace("enigma2-plugin-", "")
 
-        display = clean.replace("enigma2-plugin-", "")
+    # التحقق هل الملف محلي أم من الـ Release
+    file_path_url = f"{BASE_URL}/plugins/{filename}"
+    for asset in release_assets_pool:
+        if asset["filename"] == filename:
+            file_path_url = asset["url"]
+            break
 
-        image_name = display
-        image_name = image_name.replace("extensions-", "")
-        image_name = image_name.replace("skins-", "")
-        image_name = image_name.split("_")[0]
-
-        data["categories"]["plugins"].append({
-            "name": display,
-            "version": version,
-            "description": old_descriptions.get(display, ""),
-            "file": f"{BASE_URL}/plugins/{filename}",
-           "image": image_url(
-    display.split("_")[0]
-    .replace("extensions-", "")
-    .replace("skins-", "")
-    .replace("plugin-", "")
-) 
-        })
+    data["categories"]["plugins"].append({
+        "name": display,
+        "version": version,
+        "description": old_descriptions.get(display, ""),
+        "file": file_path_url,
+        "image": image_url(
+            display.split("_")[0]
+            .replace("extensions-", "")
+            .replace("skins-", "")
+            .replace("plugin-", "")
+        )
+    })
 
 
 # -------------------------------------------------
@@ -108,18 +141,13 @@ if os.path.isdir("plugins"):
 # -------------------------------------------------
 
 if os.path.isdir("skins"):
-
     for folder in sorted(os.listdir("skins")):
-
         folder_path = os.path.join("skins", folder)
-
         if not os.path.isdir(folder_path):
             continue
 
         items = []
-
         for filename in sorted(os.listdir(folder_path)):
-
             if not filename.endswith(EXTENSIONS):
                 continue
 
@@ -129,7 +157,6 @@ if os.path.isdir("skins"):
                 clean = os.path.splitext(filename)[0]
 
             version = clean.split("_")[-2] if "_" in clean else "1.0"
-
             image_name = clean.replace("enigma2-plugin-skins-", "")
             image_name = image_name.split("_")[0]
 
@@ -151,62 +178,80 @@ if os.path.isdir("skins"):
 # Tools
 # -------------------------------------------------
 
+tools_list = []
 if os.path.isdir("tools"):
-
     for filename in sorted(os.listdir("tools")):
+        if filename.endswith(EXTENSIONS):
+            tools_list.append(filename)
 
-        if not filename.endswith(EXTENSIONS):
-            continue
+for asset in release_assets_pool:
+    fname = asset["filename"]
+    if "ncam" in fname.lower() or "oscam" in fname.lower() or "tool" in fname.lower() or "script" in fname.lower():
+        if fname not in tools_list:
+            tools_list.append(fname)
 
-        if filename.endswith(".tar.gz"):
-            clean = filename[:-7]
-        else:
-            clean = os.path.splitext(filename)[0]
+for filename in sorted(tools_list):
+    if filename.endswith(".tar.gz"):
+        clean = filename[:-7]
+    else:
+        clean = os.path.splitext(filename)[0]
 
-        version = clean.split("_")[-2] if "_" in clean else "1.0"
+    version = clean.split("_")[-2] if "_" in clean else "1.0"
+    image_name = clean.split("_")[0]
 
-        image_name = clean.split("_")[0]
+    file_path_url = f"{BASE_URL}/tools/{filename}"
+    for asset in release_assets_pool:
+        if asset["filename"] == filename:
+            file_path_url = asset["url"]
+            break
 
-        data["categories"]["tools"].append({
-            "name": clean,
-            "version": version,
-            "description": old_descriptions.get(clean,""),
-            "file": f"{BASE_URL}/tools/{filename}",
-            "image": image_url(image_name)
-        })
+    data["categories"]["tools"].append({
+        "name": clean,
+        "version": version,
+        "description": old_descriptions.get(clean,""),
+        "file": file_path_url,
+        "image": image_url(image_name)
+    })
 
 
 # -------------------------------------------------
 # System Images
 # -------------------------------------------------
 
+sys_list = []
 if os.path.isdir("system_images"):
-
     for filename in sorted(os.listdir("system_images")):
+        if filename.endswith((".zip", ".tar.gz", ".img", ".nfi")):
+            sys_list.append(filename)
 
-        if not (
-            filename.endswith(".zip")
-            or filename.endswith(".tar.gz")
-            or filename.endswith(".img")
-        ):
-            continue
+for asset in release_assets_pool:
+    fname = asset["filename"]
+    if any(img_kw in fname.lower() for img_kw in ["egami", "openatv", "blackhole", "vti", "pure2", "image", "vu+"]):
+        if fname not in sys_list:
+            sys_list.append(fname)
 
-        if filename.endswith(".tar.gz"):
-            clean = filename[:-7]
-        else:
-            clean = os.path.splitext(filename)[0]
+for filename in sorted(sys_list):
+    if filename.endswith(".tar.gz"):
+        clean = filename[:-7]
+    else:
+        clean = os.path.splitext(filename)[0]
 
-        version = clean.split("_")[-2] if "_" in clean else "1.0"
+    version = clean.split("_")[-2] if "_" in clean else "1.0"
+    image_name = clean.split("_")[0]
 
-        image_name = clean.split("_")[0]
+    file_path_url = f"{BASE_URL}/system_images/{filename}"
+    for asset in release_assets_pool:
+        if asset["filename"] == filename:
+            file_path_url = asset["url"]
+            break
 
-        data["categories"]["system_images"].append({
-            "name": clean,
-            "version": version,
-            "description": old_descriptions.get(clean,""),
-            "file": f"{BASE_URL}/system_images/{filename}",
-            "image": image_url(image_name)
-        })
+    data["categories"]["system_images"].append({
+        "name": clean,
+        "version": version,
+        "description": old_descriptions.get(clean,""),
+        "file": file_path_url,
+        "image": image_url(image_name)
+    })
 
 
 # -------------------------------------------------
@@ -215,54 +260,28 @@ if os.path.isdir("system_images"):
 
 picons_dict = {}
 
-# 1- Read from GitHub Releases API
-try:
-    api = f"https://api.github.com/repos/{GITHUB_USER}/{REPO_NAME}/releases"
+# 1- Read from GitHub Releases Assets Pool
+for asset in release_assets_pool:
+    filename = asset["filename"]
+    if "picon" not in filename.lower():
+        continue
 
-    req = urllib.request.Request(
-        api,
-        headers={
-            "User-Agent": "Mozilla/5.0",
-            "Accept": "application/vnd.github+json"
-        }
-    )
+    if filename.endswith(".tar.gz"):
+        clean = filename[:-7]
+    else:
+        clean = os.path.splitext(filename)[0]
 
-    with urllib.request.urlopen(req, timeout=20) as response:
-        releases = json.loads(response.read().decode("utf-8"))
-
-    for release in releases:
-        for asset in release.get("assets", []):
-
-            filename = asset["name"]
-
-            # فلترة ذكية: التأكد من وجود كلمة picon في اسم الملف لمنع تداخل الإضافات والقنوات الأخرى
-            if "picon" not in filename.lower():
-                continue
-
-            if not filename.endswith(EXTENSIONS):
-                continue
-
-            if filename.endswith(".tar.gz"):
-                clean = filename[:-7]
-            else:
-                clean = os.path.splitext(filename)[0]
-
-            picons_dict[filename] = {
-                "name": clean,
-                "version": "1.0",
-                "description": old_descriptions.get(clean, ""),
-                "file": asset["browser_download_url"],
-                "image": image_url(clean.split("_")[0])
-            }
-
-except Exception as e:
-    print("GitHub Releases:", e)
+    picons_dict[filename] = {
+        "name": clean,
+        "version": "1.0",
+        "description": old_descriptions.get(clean, ""),
+        "file": asset["url"],
+        "image": image_url(clean.split("_")[0])
+    }
 
 # 2- Read local picons folder
 if os.path.isdir("picons"):
-
     for filename in sorted(os.listdir("picons")):
-
         if not filename.endswith(EXTENSIONS):
             continue
 
@@ -284,21 +303,42 @@ for filename in sorted(picons_dict):
     data["categories"]["picons"].append(picons_dict[filename])
 
 
+# -------------------------------------------------
 # Channels
+# -------------------------------------------------
+
+channels_list = []
 if os.path.isdir("channels"):
     for filename in sorted(os.listdir("channels")):
         if filename.endswith(EXTENSIONS):
-            if filename.endswith(".tar.gz"):
-                clean = filename[:-7]
-            else:
-                clean = os.path.splitext(filename)[0]
-            data["categories"]["channels"].append({
-                "name":clean,
-                "version":"1.0",
-                "description":old_descriptions.get(clean,""),
-                "file":f"{BASE_URL}/channels/{filename}",
-                "image":image_url(clean.split("_")[0])
-            })
+            channels_list.append(filename)
+
+for asset in release_assets_pool:
+    fname = asset["filename"]
+    if "channel" in fname.lower() or "backup" in fname.lower():
+        if fname not in channels_list:
+            channels_list.append(fname)
+
+for filename in sorted(channels_list):
+    if filename.endswith(".tar.gz"):
+        clean = filename[:-7]
+    else:
+        clean = os.path.splitext(filename)[0]
+
+    file_path_url = f"{BASE_URL}/channels/{filename}"
+    for asset in release_assets_pool:
+        if asset["filename"] == filename:
+            file_path_url = asset["url"]
+            break
+
+    data["categories"]["channels"].append({
+        "name": clean,
+        "version": "1.0",
+        "description": old_descriptions.get(clean,""),
+        "file": file_path_url,
+        "image": image_url(clean.split("_")[0])
+    })
+
 
 # -------------------------------------------------
 # Save JSON
