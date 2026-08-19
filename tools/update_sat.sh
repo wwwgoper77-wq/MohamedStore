9d64b16a4a10335b8e5d0784df41882c9b0df771
wwwgoper77-wq <www.goper77@gmail.com>
2026-08-19 22:40:52 +0300

    Update update_sat.sh

--- tools/update_sat.sh
+++ tools/update_sat.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-wget -q "--no-check-certificate" "https://raw.githubusercontent.com/oe-alliance/oe-alliance-plugins/master/xml/satellites.xml" -O /etc/enigma2/satellites.xml
+# تنزيل الملف وحفظه في المسار الرئيسي
+wget -q --no-check-certificate "https://raw.githubusercontent.com/oe-alliance/oe-alliance-plugins/master/xml/satellites.xml" -O /etc/tuxbox/satellites.xml
+
+# إنشاء رابط مباشر أو نسخ الملف إلى مسار enigma2 أيضاً
+cp /etc/tuxbox/satellites.xml /etc/enigma2/satellites.xml
+
+# أعطاء صلاحيات القراءة للرسيفر
+chmod 644 /etc/tuxbox/satellites.xml
+chmod 644 /etc/enigma2/satellites.xml
+
 echo "Satellites.xml Updated Successfully!"
 exit 0
