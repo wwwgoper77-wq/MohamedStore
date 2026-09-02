346fdfa783f73efd4567bd877ca02b9585ac6df9
wwwgoper77-wq <www.goper77@gmail.com>
2026-09-03 00:25:36 +0300

    Add files via upload

---
+++ tools/audio/Fox-Anis-ahmad yahia-Quran.sh
@@ -0,0 +1,288 @@
+#!/bin/sh
+# Enigma2 IPAudioPro Playlist Installer
+cat << 'EOF' > /etc/enigma2/IPAudioPro.json
+{
+    "Playlist": {
+        "streams": [
+            {
+                "name": "FoX FM Live 1",
+                "display_name": "FoX FM Live 1",
+                "url": "http://foxfm.xyz:8080/live1"
+            },
+            {
+                "name": "FoX FM Live 2",
+                "display_name": "FoX FM Live 2",
+                "url": "http://foxfm.xyz:8080/live2"
+            },
+            {
+                "name": "FoX FM Live 3",
+                "display_name": "FoX FM Live 3",
+                "url": "http://foxfm.xyz:8080/live3"
+            },
+            {
+                "name": "FoX FM Live 4",
+                "display_name": "FoX FM Live 4",
+                "url": "http://foxfm.xyz:8080/live4"
+            },
+            {
+                "name": "FoX FM 1",
+                "display_name": "FoX FM 1",
+                "url": "http://foxfm.xyz/1"
+            },
+            {
+                "name": "FoX FM 2",
+                "display_name": "FoX FM 2",
+                "url": "http://foxfm.xyz/2"
+            },
+            {
+                "name": "FoX FM 3",
+                "display_name": "FoX FM 3",
+                "url": "http://foxfm.xyz/3"
+            },
+            {
+                "name": "FoX FM 4",
+                "display_name": "FoX FM 4",
+                "url": "http://foxfm.xyz/4"
+            },
+            {
+                "name": "FoX FM 5",
+                "display_name": "FoX FM 5",
+                "url": "http://foxfm.xyz/5"
+            },
+            {
+                "name": "FoX FM 6",
+                "display_name": "FoX FM 6",
+                "url": "http://foxfm.xyz/6"
+            },
+            {
+                "name": "FoX FM 7",
+                "display_name": "FoX FM 7",
+                "url": "http://foxfm.xyz/7"
+            },
+            {
+                "name": "FoX FM 8",
+                "display_name": "FoX FM 8",
+                "url": "http://foxfm.xyz/8"
+            },
+            {
+                "name": "FoX FM 9",
+                "display_name": "FoX FM 9",
+                "url": "http://foxfm.xyz/9"
+            },
+            {
+                "name": "FoX FM Xtra 1",
+                "display_name": "FoX FM Xtra 1",
+                "url": "http://foxfm.xyz:8080/xtra1"
+            },
+            {
+                "name": "FoX FM Xtra 2",
+                "display_name": "FoX FM Xtra 2",
+                "url": "http://foxfm.xyz:8080/xtra2"
+            },
+            {
+                "name": "FoX FM Xtra 3",
+                "display_name": "FoX FM Xtra 3",
+                "url": "http://foxfm.xyz:8080/xtra3"
+            },
+            {
+                "name": "FoX FM EN 1",
+                "display_name": "FoX FM EN 1",
+                "url": "http://foxfm.xyz:8080/en1"
+            },
+            {
+                "name": "FoX FM EN 2",
+                "display_name": "FoX FM EN 2",
+                "url": "http://foxfm.xyz:8080/en2"
+            },
+            {
+                "name": "FoX FM FR 1",
+                "display_name": "FoX FM FR 1",
+                "url": "http://foxfm.xyz:8080/fr1"
+            },
+            {
+                "name": "FoX FM FR 2",
+                "display_name": "FoX FM FR 2",
+                "url": "http://foxfm.xyz:8080/fr2"
+            },
+            {
+                "name": "FoX FM StarzPlay 1",
+                "display_name": "FoX FM StarzPlay 1",
+                "url": "http://foxfm.xyz:8080/starzplay1"
+            },
+            {
+                "name": "FoX FM StarzPlay 2",
+                "display_name": "FoX FM StarzPlay 2",
+                "url": "http://foxfm.xyz:8080/starzplay2"
+            },
+            {
+                "name": "FoX FM Shasha 1",
+                "display_name": "FoX FM Shasha 1",
+                "url": "http://foxfm.xyz:8080/shasha1"
+            },
+            {
+                "name": "FoX FM Shasha 2",
+                "display_name": "FoX FM Shasha 2",
+                "url": "http://foxfm.xyz:8080/shasha2"
+            },
+            {
+                "name": "FoX FM Thmanyah 1",
+                "display_name": "FoX FM Thmanyah 1",
+                "url": "http://foxfm.xyz:8080/thmanyah1"
+            },
+            {
+                "name": "FoX FM Thmanyah 2",
+                "display_name": "FoX FM Thmanyah 2",
+                "url": "http://foxfm.xyz:8080/thmanyah2"
+            },
+            {
+                "name": "FoX FM Thmanyah 3",
+                "display_name": "FoX FM Thmanyah 3",
+                "url": "http://foxfm.xyz:8080/thmanyah3"
+            },
+            {
+                "name": "FoX FM Shahid 1",
+                "display_name": "FoX FM Shahid 1",
+                "url": "http://foxfm.xyz:8080/shahid1"
+            },
+            {
+                "name": "FoX FM Shahid 2",
+                "display_name": "FoX FM Shahid 2",
+                "url": "http://foxfm.xyz:8080/shahid2"
+            },
+            {
+                "name": "FoX FM Alkass 5",
+                "display_name": "FoX FM Alkass 5",
+                "url": "http://foxfm.xyz:8080/alkass5"
+            },
+            {
+                "name": "FoX FM Alkass 6",
+                "display_name": "FoX FM Alkass 6",
+                "url": "http://foxfm.xyz:8080/alkass6"
+            },
+            {
+                "name": "FoX FM Alkass 7",
+                "display_name": "FoX FM Alkass 7",
+                "url": "http://foxfm.xyz:8080/alkass7"
+            },
+            {
+                "name": "FoX FM Alkass 8",
+                "display_name": "FoX FM Alkass 8",
+                "url": "http://foxfm.xyz:8080/alkass8"
+            },
+            {
+                "name": "Anis1",
+                "display_name": "Anis1",
+                "url": "https://anisfm.pp.ua/1"
+            },
+            {
+                "name": "Anis1Low",
+                "display_name": "Anis1Low",
+                "url": "https://anisfm.pp.ua/1L"
+            },
+            {
+                "name": "Anis2",
+                "display_name": "Anis2",
+                "url": "https://anisfm.pp.ua/2"
+            },
+            {
+                "name": "Anis2Low",
+                "display_name": "Anis2Low",
+                "url": "https://anisfm.pp.ua/2L"
+            },
+            {
+                "name": "Anis3",
+                "display_name": "Anis3",
+                "url": "https://anisfm.pp.ua/3"
+            },
+            {
+                "name": "Anis3Low",
+                "display_name": "Anis3Low",
+                "url": "https://anisfm.pp.ua/3L"
+            },
+            {
+                "name": "Anis4",
+                "display_name": "Anis4",
+                "url": "https://anisfm.pp.ua/4"
+            },
+            {
+                "name": "Anis4Low",
+                "display_name": "Anis4Low",
+                "url": "https://anisfm.pp.ua/4L"
+            },
+            {
+                "name": "Anis5",
+                "display_name": "Anis5",
+                "url": "https://anisfm.pp.ua/5"
+            },
+            {
+                "name": "Anis5Low",
+                "display_name": "Anis5Low",
+                "url": "https://anisfm.pp.ua/5L"
+            },
+            {
+                "name": "Anis FM Max 1",
+                "display_name": "Anis FM Max 1",
+                "url": "https://anisfm.pp.ua/Mx1"
+            },
+            {
+                "name": "Ahmad Yahia 1",
+                "display_name": "Ahmad Yahia 1",
+                "url": "http://anisfm.pp.ua/ahmadyahia1#"
+            },
+            {
+                "name": "Ahmad Yahia 2",
+                "display_name": "Ahmad Yahia 2",
+                "url": "http://anisfm.pp.ua/ahmadyahia2#"
+            },
+            {
+                "name": "Ahmad Yahia 3",
+                "display_name": "Ahmad Yahia 3",
+                "url": "http://anisfm.pp.ua/ahmadyahia3#"
+            },
+            {
+                "name": "Quran Saudi",
+                "display_name": "Quran Saudi",
+                "url": "https://stream.radiojar.com/0tpy1h0kxtzuv#"
+            },
+            {
+                "name": "Quran Makkah",
+                "display_name": "Quran Makkah",
+                "url": "https://edge.mixlr.com/channel/rwumx#"
+            },
+            {
+                "name": "Tarateel Quran",
+                "display_name": "Tarateel Quran",
+                "url": "https://qurango.net/radio/tarateel#"
+            },
+            {
+                "name": "Sunnah Radio",
+                "display_name": "Sunnah Radio",
+                "url": "http://andromeda.shoutca.st:8189/live#"
+            },
+            {
+                "name": "Quran Iraq Al-Siraj",
+                "display_name": "Quran Iraq Al-Siraj",
+                "url": "http://192.99.170.8:5550/stream#"
+            },
+            {
+                "name": "Quran Cairo",
+                "display_name": "Quran Cairo",
+                "url": "https://n0a.radiojar.com/0tpy1h0kxtzuv#"
+            },
+            {
+                "name": "Abdulbasit Abdulsamad",
+                "display_name": "Abdulbasit Abdulsamad",
+                "url": "https://radio.mp3islam.com/listen/abdulbasit/radio.mp3"
+            },
+            {
+                "name": "Mohamed Siddiq Minshawi",
+                "display_name": "Mohamed Siddiq Minshawi",
+                "url": "https://radio.mp3islam.com/listen/minshawi/radio.mp3"
+            }
+        ]
+    }
+}
+EOF
+chmod 755 /etc/enigma2/IPAudioPro.json
+killall -9 enigma2
+echo "IPAudioPro.json updated successfully!"
